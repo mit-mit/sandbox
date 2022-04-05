7b593cae16fd7e63fd75d8d2b706d3b6f1600a7d
Michael Thomsen <mit@google.com>
2022-04-05 15:20:22 +0200

    Finish current

---
+++ platform_api/current_platform/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:platform/platform.dart' as platform;
+
+void main() {
+  runApp(const MaterialApp(title: 'Flutter Demo', home: MyHomePage()));
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Platform test')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            ProductWidget(),
+            HostnameWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductWidget extends StatelessWidget {
+  const ProductWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final isMac = const platform.LocalPlatform().isMacOS;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        for (var product in ['Foo1', 'Foo2', 'Foo3'])
+          isMac ? Text('i$product') : Text(product)
+      ],
+    );
+  }
+}
+
+class HostnameWidget extends StatelessWidget {
+  const HostnameWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final hostName = const platform.LocalPlatform().localHostname;
+
+    return Text('Host name: $hostName');
+  }
+}
