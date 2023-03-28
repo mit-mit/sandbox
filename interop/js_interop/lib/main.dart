4b5759f75ca7e6fbbc1687d41526688ccdc53168
Michael Thomsen <mit@google.com>
2023-03-28 23:52:58 +0200

    Old style too

--- interop/js_interop/lib/main.dart
+++ interop/js_interop/lib/main.dart
@@ -1,18 +1,21 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as dart_js;
 import 'package:flutter/material.dart';
 import 'js/date.dart' as js;
 import 'js/navigator.dart' as js;
 
 void main() async {
-  runApp(const MaterialApp(home: MyHomePage()));
+  runApp(const MaterialApp(home: JSInteropNew()));
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class JSInteropNew extends StatelessWidget {
+  const JSInteropNew({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final onlineStatus = js.window.navigator.onLine;
-    final memory = js.window.navigator.deviceMemory;
+    final navigator = js.window.navigator;
+    final onlineStatus = navigator.onLine;
+    final memory = navigator.deviceMemory;
 
     final now = js.Date.current();
 
@@ -25,6 +28,31 @@ class MyHomePage extends StatelessWidget {
             Text('Browser is ${onlineStatus ? 'online' : 'offline'}'),
             Text('System has $memory GB memory.'),
             Text('Today is ${now.toLocaleDateString('en-US')}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class JSInteropOld extends StatelessWidget {
+  const JSInteropOld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final navigator =
+        dart_js.JsObject.fromBrowserObject(dart_js.context['navigator']);
+    final onlineStatus = navigator['onLine'];
+    final memory = navigator['deviceMemory'];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('JS interop vNext')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Browser is ${onlineStatus ? 'online' : 'offline'}'),
+            Text('System has $memory GB memory.'),
           ],
         ),
       ),
