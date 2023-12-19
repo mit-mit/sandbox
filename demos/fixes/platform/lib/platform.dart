2d187a2890902077292ffc1a9e0c9f51910f985c
Michael Thomsen <mit@google.com>
2023-12-19 12:44:52 +0100

    Tweaks

--- demos/fixes/platform/lib/platform.dart
+++ demos/fixes/platform/lib/platform.dart
@@ -1,5 +1,16 @@
 class LocalPlatform {
-  @Deprecated('Do not use')
+  HostPlatform _host = HostPlatform.current;
+
+  @Deprecated('Use HostPlatform')
+  bool get isAndroid => _host.isAndroid;
+  @Deprecated('Use NativePlatform')
+  String get localHostname => '';
+}
+
+class HostPlatform {
   bool get isAndroid => true;
+}
+
+class NativePlatform {
   String get localHostname => '';
 }
