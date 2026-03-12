c6eb310cc5efbe7c1fa9e1fa1305c7f7597791e5
Michael Thomsen <mit@google.com>
2026-03-12 13:26:28 +0100

    Primary constructors working

--- future/primary-constructor/primary_constructor.dart
+++ future/primary-constructor/primary_constructor.dart
@@ -1,34 +1,21 @@
-void main(List<String> arguments) {
-  //
-  // Point.
-  //
-
-  final p = Point(12,42);
-  print('Point p: $p');
-
-  //
-  // Vehicle.
-  //
-
-  var cart = Vehicle('My red cart', 2);
-  print(cart.describe);
-
-  // This currently fails, skip for now.
-  //cart.name = 'My old cart';
-  //print(cart.describe);
-
-  final rocket = Vehicle.unwheeled('My spaceship');
-  print(rocket.describe);
-}
-
 class Point(final int x, final int y);
 
 class Vehicle(var String name, final int wheels) {
   Vehicle.unwheeled(String name) : this(name, -1);
 
-  String get describe {
-    String description = name;
-    description+= ' has ${ wheels == -1 ? 'no wheels' : '$wheels wheels' }';
-    return description;
-  }
+  String get describe => '$name has ${ wheels == -1 ? 'no wheels' : '$wheels wheels' }';
+}
+
+void main(List<String> arguments) {
+  final p = Point(12,42);
+  print('Point p: ${p.x},${p.y}');
+
+  var cart = Vehicle('My red cart', 2);
+  print(cart.describe);
+
+  cart.name = 'My old cart';
+  print(cart.describe);
+
+  final rocket = Vehicle.unwheeled('My spaceship');
+  print(rocket.describe);
 }
